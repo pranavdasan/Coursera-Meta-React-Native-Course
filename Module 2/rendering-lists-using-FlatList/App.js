5cd80467ebc39cc354e5e96e340ab3102c8c35a4
Pranav Dasan <pranavdasan8@gmail.com>
2024-07-15 14:42:58 -0700

    Completed 'rendering-lists-using-FlatList' project

---
+++ Module 2/rendering-lists-using-FlatList/App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import MenuItems from './components/MenuItems';
+
+export default function App() {
+  return (
+    <View style={appStyles.container}>
+
+      <LittleLemonHeader />
+
+      <View style={appStyles.menuContainer}>
+        <MenuItems />
+      </View>
+
+      <LittleLemonFooter />
+
+    </View>
+  );
+}
+
+const appStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  menuContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#333333',
+  }
+})
---
+++ Module 2/rendering-lists-using-FlatList/README.md
@@ -0,0 +1,3 @@
+# Project Description
+In this project, you learn about the `<FlatList>` componen, how it uses lazy loading to load large lists faster, and how to format it using both styles and props.
+
